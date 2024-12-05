d983206202d920fe893f2f80353ea6ce9473ee53
Dhaval Solanki <dhavalas24@gmail.com>
2024-12-05 11:57:25 +0530

    05 Dec

--- my-Project/src/pages/Error404page.jsx
+++ my-Project/src/pages/Error404page.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const Error404page = () => {
   return (
@@ -8,7 +9,7 @@ const Error404page = () => {
     probably moved somewhere
     </div>
     <p className='mt-4 text-[#808080] flex justify-center items-center text-center'>Please back  to homepage or check  our offer</p>
-    <a href="https://www.google.com"><button className="bg-[#0068C8] text-[#fff] border-1 border-[#0068C8] hover:bg-[#fff] hover:text-[#0068C8] transition duration-300 mt-3 font-bold px-7 py-2 lg:px-10 lg:py-2.5 rounded-lg uppercase">Back to homepage</button></a>
+    <Link to='/'><button className="bg-[#0068C8] text-[#fff] border-1 border-[#0068C8] hover:bg-[#fff] hover:text-[#0068C8] transition duration-300 mt-3 font-bold px-7 py-2 lg:px-10 lg:py-2.5 rounded-lg uppercase">Back to homepage</button></Link>
 </div>
   )
 }
